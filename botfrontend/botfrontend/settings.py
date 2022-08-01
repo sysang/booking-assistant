f1c0ded8cd476b9e40d0993aa5491cae3c24cdc5
sysang <sysangtiger@gmail.com>
2022-08-01 16:27:26 +0000

    Checked.

--- botfrontend/botfrontend/settings.py
+++ botfrontend/botfrontend/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'dialogue/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
